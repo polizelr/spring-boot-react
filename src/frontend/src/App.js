43ecfe71c6acdf730aeb79fddb9744a697131e89
Rafaela Polizel <polizelc.rafaela@gmail.com>
2021-03-25 17:42:00 -0300

    Adding proxy requests to backend

--- src/frontend/src/App.js
+++ src/frontend/src/App.js
@@ -1,12 +1,16 @@
 import './App.css';
 import {Button} from 'antd';
+import {getAllStudents} from "./client";
 
 function App() {
-  return (
-    <div className="App">
-      <Button type='primary'>Hello</Button>
-    </div>
-  );
+    getAllStudents()
+        .then(res => res.json())
+        .then(console.log)
+    return (
+        <div className="App">
+            <Button type='primary'>Hello</Button>
+        </div>
+    );
 }
 
 export default App;

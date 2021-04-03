0aa94ac4959b8984419245df6aa8464d432bea66
Rafaela Polizel <polizelc.rafaela@gmail.com>
2021-04-02 22:59:41 -0300

    Loading indicator and reload table

--- src/frontend/src/StudentDrawerForm.js
+++ src/frontend/src/StudentDrawerForm.js
@@ -1,19 +1,29 @@
-import {Drawer, Input, Col, Select, Form, Row, Button} from 'antd';
+import {Drawer, Input, Col, Select, Form, Row, Button, Spin} from 'antd';
 
 import { addNewStudent } from "./client";
+import {LoadingOutlined} from "@ant-design/icons";
+import {useState} from "react";
 
 const {Option} = Select;
 
-function StudentDrawerForm({showDrawer, setShowDrawer}) {
+const antIcon = <LoadingOutlined style={{ fontSize: 24 }} spin />;
+
+function StudentDrawerForm({showDrawer, setShowDrawer, fetchStudents}) {
     const onCLose = () => setShowDrawer(false);
+    const [submitting, setSubmitting] = useState(false);
 
     const onFinish = student => {
+        setSubmitting(true);
         console.log(JSON.stringify(student, null, 2));
         addNewStudent(student)
             .then(()=>{
                 console.log("student added");
+                onCLose();
+                fetchStudents();
             }).catch(err => {
                 console.log(err);
+            }).finally(()=>{
+                setSubmitting(false);
         })
     };
 
@@ -86,6 +96,9 @@ function StudentDrawerForm({showDrawer, setShowDrawer}) {
                         </Button>
                     </Form.Item>
                 </Col>
+            </Row>
+            <Row>
+                {submitting && <Spin indicator={antIcon} />}
             </Row>
         </Form>
     </Drawer>
